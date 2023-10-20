ab449587c998ae861a9e3bbc2835338116ad23d6
Hammad Afzal <hammadafzal05@gmail.com>
2023-10-21 00:05:55 +0100

    Add more jsx in men's category

--- src/components/men category/MenProductListing.jsx
+++ src/components/men category/MenProductListing.jsx
@@ -30,15 +30,35 @@ const menBags = [
 ];
 function MenProductListing() {
   return (
-    <ul className={styles.cardContainer}>
-      {menBags.map((bag) => (
-        <li key={bag.id}>
-          <img src={bag.image1} alt="bag image" />
-          <p>{bag.price}</p>
-          <h3>{bag.name}</h3>
-        </li>
-      ))}
-    </ul>
+    <>
+      <Heading />
+      <ul className={styles.cardContainer}>
+        {menBags.map((bag) => (
+          <li key={bag.id} className={styles.card}>
+            <img src={bag.image1} alt="bag image" />
+            <span className={styles.arrowLeft}>&#10095;</span>
+            <span className={styles.arrowRight}>&#10094;</span>
+            <p>{bag.price}</p>
+            <h3>{bag.name}</h3>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+function Heading() {
+  return (
+    <div>
+      <h1>Men's Bags</h1>
+      <p>
+        From the office to the gym to weekends away, look sleek on every
+        occasion with a piece from our collection of men's bags. Choose from
+        rucksacks in water-resistant materials with convenient laptop pockets
+        and classic leather messenger bags in a timeless brown or black. Our
+        bumbags give you maximum security for your essentials, whether
+        jet-setting or heading to the shops
+      </p>
+    </div>
   );
 }
 export default MenProductListing;
