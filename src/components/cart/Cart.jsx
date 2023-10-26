1d35c833c4f414b7c89c5572e497e40f98a6b9cf
Hammad Afzal <hammadafzal05@gmail.com>
2023-10-26 03:21:12 +0100

    More work on cart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -5,24 +5,34 @@ function Cart() {
   const [open, setOpen] = useState(false);
   function handleToggleModal() {
     setOpen(!open);
-    console.log(open ? "Open" : "close");
+    console.log(open ? "close" : "open");
   }
-
+  function handleCloseOnDocument() {
+    setOpen(false);
+  }
   return (
     <>
-      <button onClick={handleToggleModal}>Basket</button>
+      <button onClick={handleToggleModal} className={styles.basketBtn}>
+        Basket
+      </button>
       {open && (
-        <div className={styles.cardContainer}>
-          <h1>Hi</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis nulla
-            ipsam temporibus maxime optio consectetur molestiae eaque. Quas
-            maiores sed deserunt, voluptatibus sequi alias eum, voluptate
-            consequatur molestias esse expedita.
-          </p>
+        <div className={styles.cartContainer}>
+          <h1 onMouseDown={handleCloseOnDocument}>Your Items</h1>
+          <hr />
+          <img src="../public/images/menbags/backpack-front.jpg" />
+          <p>Backpack Front Pocket</p>
+          <span>$20</span>
+          <button>Remove Item</button>
+          <hr />
+          <img src="../public/images/menbags/his-carry-all.jpg" />
+          <p>Backpack Side Pocket</p>
+          <span>$35</span>
+          <button>Remove Item</button>
+          <a href="/checkout">Check Out Now!</a>
+
           <div className={styles.cartContent}></div>
           <button className={styles.button} onClick={handleToggleModal}>
-            &times; afjlnsafjnl
+            &times; Close
           </button>
         </div>
       )}
